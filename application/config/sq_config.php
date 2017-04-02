b764be7e49124845e73211774578bef50cdee473
janmonteros <janraymonteros@gmail.com>
2017-04-02 23:50:30 +0800

    Change DB Name from assessment to assessment_dev

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -1,7 +1,7 @@
 <?php
 $sq_base_url='http://localhost/assessment/';
 $sq_hostname='localhost';
-$sq_dbname='assessment';
+$sq_dbname='assessment_dev';
 $sq_dbusername='root';
 $sq_dbpassword='';
 ?>
