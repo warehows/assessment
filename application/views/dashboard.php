7143469f40535bc2d469fa60a57fe27472489b87
Joeven <cervezajoeven@gmail.com>
2017-04-29 23:18:18 +0800

    - Remove Savsoft on footer.

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -6,30 +6,30 @@
 
     <div class="row">
 
-        <div class="col-md-4">
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-xs-3">
-                            <i class="fa fa-users fa-5x"></i>
-                        </div>
-                        <div class="col-xs-9 text-right">
-                            <div class="huge"><?php echo $num_users; ?></div>
-                            <div><?php echo $this->lang->line('no_registered_user'); ?> </div>
-                        </div>
-                    </div>
-                </div>
-                <a href="<?php echo site_url('user'); ?>">
-                    <div class="panel-footer">
-                        <span
-                            class="pull-left"><?php echo $this->lang->line('users'); ?><?php echo $this->lang->line('list'); ?></span>
-                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-
-                        <div class="clearfix"></div>
-                    </div>
-                </a>
-            </div>
-        </div>
+<!--        <div class="col-md-4">-->
+<!--            <div class="panel panel-info">-->
+<!--                <div class="panel-heading">-->
+<!--                    <div class="row">-->
+<!--                        <div class="col-xs-3">-->
+<!--                            <i class="fa fa-users fa-5x"></i>-->
+<!--                        </div>-->
+<!--                        <div class="col-xs-9 text-right">-->
+<!--                            <div class="huge">--><?php //echo $num_users; ?><!--</div>-->
+<!--                            <div>--><?php //echo $this->lang->line('no_registered_user'); ?><!-- </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <a href="--><?php //echo site_url('user'); ?><!--">-->
+<!--                    <div class="panel-footer">-->
+<!--                        <span-->
+<!--                            class="pull-left">--><?php //echo $this->lang->line('users'); ?><!----><?php //echo $this->lang->line('list'); ?><!--</span>-->
+<!--                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>-->
+<!---->
+<!--                        <div class="clearfix"></div>-->
+<!--                    </div>-->
+<!--                </a>-->
+<!--            </div>-->
+<!--        </div>-->
 
 
         <div class="col-md-4">
