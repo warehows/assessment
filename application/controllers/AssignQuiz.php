63d09ce844401f3922d3fd84f0be2f399796790b
Joeven <cervezajoeven@gmail.com>
2017-05-01 09:01:40 +0800

    - New filename

--- application/controllers/AssignQuiz.php
+++ application/controllers/AssignQuiz.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class assign_quiz extends CI_Controller {
+class AssignQuiz extends CI_Controller {
 
     function __construct()
     {
