14f41cd0f6237ce3a5b318135b87fb34537936eb
cervezajoeven <cervezajoeven@gmail.com>
2017-07-08 19:40:22 +0800

    referenced jquery

--- application/views/new_material/header.php
+++ application/views/new_material/header.php
@@ -14,18 +14,18 @@
     <link href="<?php echo base_url('css/new_material/css/MUSA_form-wizard.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('css/new_material/css/styles.css'); ?>" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-
+    <script type="text/javascript" src="<?php echo base_url('js/jquery.js'); ?>"></script>
 </head>
 
 <body>
-
 
 <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean">
     <div class="container">
         <div class="navbar-header"><a href="<?php echo site_url('dashboard');?>"> <img style="height: 45px!important;" class="navbar-brand image_logo" src="<?php echo base_url('css/new_material/img/main_logo.png'); ?>" alt="School Logo" >
             <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button></a>
         </div>
-        <div class="collapse navbar-collapse" id="navcol-1">
+        <div class="collapse n
+        avbar-collapse" id="navcol-1">
             <ul class="nav navbar-nav navbar-right">
                 <li role="presentation"><a href="<?php echo site_url('workspace');?>">Lesson Bank</a></li>
                 <li role="presentation"><a href="<?php echo site_url('lessons');?>">My Lesson</a></li>
@@ -43,7 +43,6 @@
                         <li role="presentation"><a href="<?php echo site_url('user/group_list');?>">Group</a></li>
                     </ul>
                 </li>
-
                 <li role="presentation"><a href="<?php echo site_url('user/logout');?>">Logout</a></li>
             </ul>
         </div>
