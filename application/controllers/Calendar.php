7ee735089807c7a1697f61598e3f27405cc933ca
cervezajoeven <cervezajoeven@gmail.com>
2017-07-09 22:22:20 +0800

    filter event

--- application/controllers/Calendar.php
+++ application/controllers/Calendar.php
@@ -11,12 +11,13 @@ class Calendar extends CI_Controller
         $this->lang->load('basic', $this->config->item('language'));
         $this->load->model("calendar_model");
         $this->load->model("grades_model");
-        $this->load->model("lessons_model");
+        $this->load->model("subjects_model");
         $this->load->model("class_model");
+        $this->load->model("user_model");
+        $this->load->model("group_model");
     }
 
     public function index(){
-        // redirect if not loggedin
         if (!$this->session->userdata('logged_in')) {
             redirect('login');
         }
@@ -25,11 +26,15 @@ class Calendar extends CI_Controller
             $this->session->unset_userdata('logged_in');
             redirect('login');
         }
+
         $logged_in = $this->session->userdata('logged_in');
         $data['logged_in'] = $logged_in;
-        $data['grade'] = $this->grades_model->all();
-        $data['lesson'] = $this->lessons_model->all_lessons_non_duplicated();
-        
+
+        $data['teachers'] = $this->user_model->get_user_by_account_type(2);
+        $data['grades'] = $this->grades_model->all();
+        $data['section'] = $this->group_model->get_all();
+        $data['subject'] = $this->subjects_model->all();
+
         if ($logged_in["su"] == 1) {
             $this->load->view('new_material/header', $data);
             $this->load->view('calendar/calendar', $data);
@@ -59,13 +64,13 @@ class Calendar extends CI_Controller
 
     public function save() {
         $data = $this->input->post();
-        $this->calendar_model->create_schedule($data); 
+        $this->calendar_model->create_schedule($data);
         redirect(site_url()."/calendar");
     }
 
     public function update() {
         $data = $this->input->post();
-        $this->calendar_model->update_schedule($data); 
+        $this->calendar_model->update_schedule($data);
     }
 
     public function delete() {
@@ -74,24 +79,34 @@ class Calendar extends CI_Controller
     }
 
     public function getEvents(){
-        $logged_in = $this->session->userdata('logged_in');
-        $data['calendar'] = $this->calendar_model->get_all($logged_in);
-        foreach ($data['calendar'] as &$row) {
-            $start = new DateTime($row['date_from']);
-            $end = new DateTime($row['date_to']);
+        $filter = $this->input->post('filter');
+        $teacher = $this->input->post('teacher');
+        $logged_in = $this->session->userdata('logged_in')['uid'];
+        $user = $filter === "true" ? $teacher : $logged_in;
+        $grade = $this->input->post('grade') == "" ? "" : $this->input->post('grade');
+        $section = $this->input->post('section') == "" ? "" : $this->input->post('section');
+        $subject = $this->input->post('subject') == "" ? "" : $this->input->post('subject');
 
-            $result[] = array(    
-                'id' => $row['calendar_id'],
-                'title' => $row['lesson_name'],
-                'section' => $row['gid'],
-                'grade' => $row['lid'],
-                'lesson' => $row['lesson_id'],
-                'start' => $row['date_from']."T00:00:00",
-                'end' => $row['date_to']."T24:00:00",
-                'color' => $row['color'],
-                'allDay' => true,
-                'editable' => true
-            );
+        $data['calendar'] = $this->calendar_model->get_all($user, $grade, $section, $subject, $filter);
+        $result = [];
+        if ($data['calendar']) {
+            foreach ($data['calendar'] as &$row) {
+                $start = new DateTime($row['date_from']);
+                $end = new DateTime($row['date_to']);
+
+                $result[] = array(
+                    'id' => $row['calendar_id'],
+                    'title' => $row['lesson_name'],
+                    'section' => $row['gid'],
+                    'grade' => $row['lid'],
+                    'lesson' => $row['lesson_id'],
+                    'start' => $row['date_from']."T00:00:00",
+                    'end' => $row['date_to']."T24:00:00",
+                    'color' => $row['color'],
+                    'allDay' => true,
+                    'editable' => true
+                );
+            }
         }
 
         echo json_encode($result);
@@ -102,9 +117,9 @@ class Calendar extends CI_Controller
         $data = $this->calendar_model->get_section($grade);
         foreach ($data as &$row) {
             $result[] = array(
-                    'gid' => $row['gid'],
-                    'group_name' => $row['group_name']
-                );
+                'gid' => $row['gid'],
+                'group_name' => $row['group_name']
+            );
         }
         echo json_encode($result);
     }
@@ -115,9 +130,9 @@ class Calendar extends CI_Controller
         $result = [];
         foreach ($data as &$row) {
             $result[] = array(
-                    'id' => $row['lid'],
-                    'name' => $row['level_name']
-                );
+                'id' => $row['lid'],
+                'name' => $row['level_name']
+            );
         }
         echo json_encode($result);
     }
@@ -128,13 +143,15 @@ class Calendar extends CI_Controller
         $result = [];
         foreach ($data as &$row) {
             $result[] = array(
-                    'id' => $row['cid'],
-                    'name' => $row['category_name']
-                );
+                'id' => $row['cid'],
+                'name' => $row['category_name']
+            );
         }
         echo json_encode($result);
     }
 
+    public function filterCalendar() {
 
+    }
 
 }
