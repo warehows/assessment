d8a5587e58cd6a252fb2f638c1a1ff7f2b732c81
Joeven <cervezajoeven@gmail.com>
2017-07-15 08:20:41 +0800

    - Quiz

--- application/controllers/Quiz.php
+++ application/controllers/Quiz.php
@@ -20,6 +20,7 @@ class Quiz extends CI_Controller
         // redirect if not loggedin
         if (!$this->session->userdata('logged_in')) {
             redirect('login');
+
         }
         $logged_in = $this->session->userdata('logged_in');
         if ($logged_in['base_url'] != base_url()) {
