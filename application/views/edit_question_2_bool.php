a69722f352c44792b1aec12d7779485cf593d658
janmonteros <janraymonteros@gmail.com>
2017-07-20 22:55:25 +0800

    change checkbox to radio

--- application/views/edit_question_2_bool.php
+++ application/views/edit_question_2_bool.php
@@ -75,7 +75,7 @@ True Or False
 			?>
 			<div class="form-group">	 
 					<label for="inputEmail"  ><?php echo $this->lang->line('options');?> <?php echo $key+1;?>)</label> <br>
-					<input type="checkbox" name="score[]" value="<?php echo $key;?>" <?php if($val['score']>=0.1){ echo 'checked'; } ?> > Select Correct Option 
+					<input type="radio" name="score[]" value="<?php echo $key;?>" <?php if($val['score']>=0.1){ echo 'checked'; } ?> > Select Correct Option
 					<br><textarea disabled name="option[]"  class="form-control"  ><?php echo $val['q_option'];?></textarea>
 			</div>
 		<?php 
