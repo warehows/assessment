bcd534f0ed63e9fddca820330e99a4601067ac01
janmonteros <janraymonteros@gmail.com>
2017-09-07 02:48:06 +0800

    Config revert

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -1,7 +1,7 @@
 <?php
-$sq_base_url='http://localhost/brainee/';
+$sq_base_url='http://warehows.net/develop/assessment/';
 $sq_hostname='localhost';
-$sq_dbname='assessment_dev8';
-$sq_dbusername='root';
-$sq_dbpassword='';
+$sq_dbname='assessment_dev';
+$sq_dbusername='jmonteros';
+$sq_dbpassword='!!pa55word';
 ?>
