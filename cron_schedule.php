358c0643ddbc224c72ae5633c5062f5865e98d0e
okong <cervezajoeven@gmail.com>
2018-04-02 00:05:18 +0800

    joeven3

--- cron_schedule.php
+++ cron_schedule.php
@@ -61,7 +61,7 @@ foreach ($result as $result_key => $result_value) {
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function(){
-        location.reload();
+        setTimeout(location.reload(), 5000);
     });
 </script>
 
