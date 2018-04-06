ca52f56e07ae6df36e00a2c27eeed8c5b2235c3c
okong <cervezajoeven@gmail.com>
2018-04-06 21:08:08 +0800

    New changelist

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -1,7 +1,20 @@
 <?php
-$sq_base_url='http://192.168.2.17/brainee/';
-$sq_hostname='localhost';
-$sq_dbname='brainee2';
-$sq_dbusername='root';
-$sq_dbpassword='!!p@ssw0rd';
+$actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$host = $_SERVER['HTTP_HOST'];
+if($host=="localhost"){
+    $base_url='http://localhost/brainee/';
+    $dbase='brainee3';
+}if($host=="192.168.2.17"){
+    $base_url='http://192.168.2.17/brainee/';
+    $dbase='brainee2';
+}else{
+    $base_url='http://warehows.net/develop/brainee/';
+    $dbase='brainee3';
+}
+$sq_base_url=$base_url;
+$sq_hostname='166.62.10.139';
+$sq_dbname=$dbase;
+$sq_dbusername='ben';
+$sq_dbpassword='Resty!Joeven241';
+
 ?>
