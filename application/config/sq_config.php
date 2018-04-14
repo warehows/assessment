3c1bd0dfb923a8dd1e777356b2a26fb97adfecca
okong <cervezajoeven@gmail.com>
2018-04-14 14:05:34 +0800

    joeven 4

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -1,14 +1,14 @@
 <?php
 $actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $host = $_SERVER['HTTP_HOST'];
-if($host=="localhost"){
-    $base_url='http://localhost/brainee/';
+if($host=="http://warehows.net"){
+    $base_url='http://warehows.net/develop/brainee/';
     $dbase='brainee3';
 }if($host=="192.168.2.17"){
     $base_url='http://192.168.2.17/brainee/';
     $dbase='brainee2';
 }else{
-    $base_url='http://warehows.net/develop/brainee/';
+    $base_url='http://localhost/brainee/';
     $dbase='brainee3';
 }
 $sq_base_url=$base_url;
@@ -16,5 +16,12 @@ $sq_hostname='166.62.10.139';
 $sq_dbname=$dbase;
 $sq_dbusername='ben';
 $sq_dbpassword='Resty!Joeven241';
+
+//$sq_base_url=$base_url;
+//$sq_hostname='localhost';
+//$sq_dbname='brainee2';
+//$sq_dbusername='root';
+//$sq_dbpassword='';
+
 
 ?>
